c124748a04487785583acde0f3490c1230a4e210
YALLICO OWNER <yallico2024@gmail.com>
2026-08-03 08:51:30 -0500

    Actualizar _ver1.js

--- plugins/_ver1.js
+++ plugins/_ver1.js
@@ -1,32 +1,27 @@
 import fs from 'fs'
 const path = './database/horarios.json'
 
-let horarios = { lunes: [], martes: [], miercoles: [], jueves: [], viernes: [], sabado: [] }
-if (fs.existsSync(path)) {
-    try { horarios = JSON.parse(fs.readFileSync(path)) } catch {}
-}
+let db = { lunes: [], martes: [], miercoles: [], jueves: [], viernes: [], sabado: [] }
+if (fs.existsSync(path)) try { db = JSON.parse(fs.readFileSync(path)) } catch {}
 
-const dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado"]
-const nombres = {lunes:"Lunes", martes:"Martes", miercoles:"Miércoles", jueves:"Jueves", viernes:"Viernes", sabado:"Sábado"}
+const nombres = {lunes:'Lunes', martes:'Martes', miercoles:'Miércoles', jueves:'Jueves', viernes:'Viernes', sabado:'Sábado'}
 const tag = jid => '@' + jid.split('@')[0]
 
 let handler = async (m, { conn }) => {
     let txt = `*📅 HORARIO DE IAS LUNES A SÁBADO*\n\n`
     let todos = []
-    dias.forEach(d => {
+    Object.keys(db).map(d => {
         txt += `*${nombres[d]}:*\n`
-        let personas = Array.isArray(horarios[d])? horarios[d] : []
-        if (personas.length > 0) {
-            personas.forEach(p => { txt += `> ${tag(p)}\n`; todos.push(p) })
+        if (db[d].length > 0) {
+            db[d].map(j => { txt += `> ${tag(j)}\n`; todos.push(j) })
         } else txt += `> Sin asignar\n`
         txt += `\n`
     })
-    return await conn.sendMessage(m.chat, { text: txt, mentions: [...new Set(todos)] }, { quoted: m })
+    await conn.sendMessage(m.chat, { text: txt, mentions: [...new Set(todos)] }, { quoted: m })
 }
 
 handler.help = ['ver1']
 handler.tags = ['horario']
-handler.command = ['ver1']
+handler.command = /^ver1$/i
 handler.group = true
-
 export default handler
