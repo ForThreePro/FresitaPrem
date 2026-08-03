e10705e5b51303d01fe0a39f9754a85bea6d3833
YALLICO OWNER <yallico2024@gmail.com>
2026-08-03 08:53:51 -0500

    Actualizar _dias.js

--- plugins/_dias.js
+++ plugins/_dias.js
@@ -1,22 +1,43 @@
-import fs from 'fs'
-const path = './database/horarios.json'
+let handler = async (m, { conn, command, usedPrefix, isAdmin }) => {
+    if (!isAdmin) return m.reply('~*~*~*~*~*~*~*~*~*~\n ACCESO DENEGADO\n~*~*~*~*~*~*~*~*~*~')
 
-let db = { lunes: [], martes: [], miercoles: [], jueves: [], viernes: [], sabado: [] }
-if (fs.existsSync(path)) try { db = JSON.parse(fs.readFileSync(path)) } catch {}
+    global.db.data.sorteos = global.db.data.sorteos || {}
+    let sorteos = global.db.data.sorteos
+    let chatId = m.chat
 
-const dias = ['lunes','martes','miercoles','jueves','viernes','sabado']
-const nombres = {lunes:'Lunes', martes:'Martes', miercoles:'Miércoles', jueves:'Jueves', viernes:'Viernes', sabado:'Sábado'}
-const tag = jid => '@' + jid.split('@')[0]
+    const dias = ['lunes','martes','miercoles','jueves','viernes','sabado']
+    const emojis = {lunes:'🌙', martes:'🌌', miercoles:'✨', jueves:'🌠', viernes:'💫', sabado:'👑'}
+    const aurora = '~*~*~*~*~*~*~*~*~*~'
+    let pp = await conn.profilePictureUrl(chatId, 'image').catch(_ => 'https://i.imgur.com/8K2JhZQ.jpg')
 
-let handler = async (m, { conn, command }) => {
-    if (db[command].length == 0) return m.reply(`*${nombres[command]}*\nSin IA asignada`)
+    let dia = command.toLowerCase()
+    if (!dias.includes(dia)) return
 
-    let lista = db[command].map(tag).join('\n')
-    await conn.sendMessage(m.chat, { text: `*${nombres[command]}*\nIA Asignada:\n${lista}`, mentions: db[command] }, { quoted: m })
+    let asignados = sorteos[chatId][dia]
+    if (!asignados ||!asignados.length) return m.reply(`${aurora}\n CIELO VACÍO\nUsa: ${usedPrefix}set${dia} @user\n${aurora}`)
+
+    const crearLista = (arr) => arr.map((u, i) => `✨ ${i+1}. @${u.split('@')[0]}`).join('\n')
+    let list = crearLista(asignados)
+    let msg = `${aurora}
+    AURORA ${dia.toUpperCase()}
+${aurora}
+
+🌌 Estado: BRILLANDO
+${emojis[dia]} Hoy te toca ${dia.toUpperCase()} ${emojis[dia]}
+
+✧ CONSTELACIÓN ✧
+${list}
+
+~* Tareas estelares *~
+1. Realizar sorteo
+2. Pedir reacciones
+3. Compartir evidencia
+
+Que tu luz ilumine el grupo ✨`
+    await conn.sendMessage(m.chat, { image: { url: pp }, caption: msg, mentions: asignados })
 }
 
-handler.help = ['lunes']
-handler.tags = ['horario']
 handler.command = /^(lunes|martes|miercoles|jueves|viernes|sabado)$/i
 handler.group = true
+handler.admin = true
 export default handler
