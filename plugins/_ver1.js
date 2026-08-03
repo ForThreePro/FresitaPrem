b38f7ab2eae47d678082210fb6bee478ae5eb57d
YALLICO OWNER <yallico2024@gmail.com>
2026-08-03 08:54:23 -0500

    Actualizar _ver1.js

--- plugins/_ver1.js
+++ plugins/_ver1.js
@@ -1,27 +1,31 @@
-import fs from 'fs'
-const path = './database/horarios.json'
-
-let db = { lunes: [], martes: [], miercoles: [], jueves: [], viernes: [], sabado: [] }
-if (fs.existsSync(path)) try { db = JSON.parse(fs.readFileSync(path)) } catch {}
-
-const nombres = {lunes:'Lunes', martes:'Martes', miercoles:'Miércoles', jueves:'Jueves', viernes:'Viernes', sabado:'Sábado'}
-const tag = jid => '@' + jid.split('@')[0]
-
 let handler = async (m, { conn }) => {
-    let txt = `*📅 HORARIO DE IAS LUNES A SÁBADO*\n\n`
+    global.db.data.sorteos = global.db.data.sorteos || {}
+    let sorteos = global.db.data.sorteos
+    let chatId = m.chat
+
+    const dias = ['lunes','martes','miercoles','jueves','viernes','sabado']
+    const emojis = {lunes:'🌙', martes:'🌌', miercoles:'✨', jueves:'🌠', viernes:'💫', sabado:'👑'}
+    const aurora = '~*~*~*~*~*~*~*~*~*~'
+    let pp = await conn.profilePictureUrl(chatId, 'image').catch(_ => 'https://i.imgur.com/8K2JhZQ.jpg')
+
+    let diasConData = dias.filter(d => Array.isArray(sorteos[chatId][d]) && sorteos[chatId][d].length > 0)
+    if (diasConData.length === 0) return m.reply(`${aurora}\n CIELO NOCTURNO VACÍO\n${aurora}`)
+
+    let txt = `${aurora}
+   CIELO SEMANAL
+${aurora}\n`
+
     let todos = []
-    Object.keys(db).map(d => {
-        txt += `*${nombres[d]}:*\n`
-        if (db[d].length > 0) {
-            db[d].map(j => { txt += `> ${tag(j)}\n`; todos.push(j) })
-        } else txt += `> Sin asignar\n`
-        txt += `\n`
-    })
-    await conn.sendMessage(m.chat, { text: txt, mentions: [...new Set(todos)] }, { quoted: m })
+    for(let d of dias){
+        if(!Array.isArray(sorteos[chatId][d]) || sorteos[chatId][d].length === 0) continue
+        txt += `\n🌌 ${emojis[d]} ${d.toUpperCase()}\n`
+        txt += sorteos[chatId][d].map((u, i) => `✨ ${i+1}. @${u.split('@')[0]}`).join('\n') + '\n'
+        todos.push(...sorteos[chatId][d])
+    }
+    txt += `\n~* que las auroras los guíen *~`
+    await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt, mentions: [...new Set(todos)] })
 }
 
-handler.help = ['ver1']
-handler.tags = ['horario']
 handler.command = /^ver1$/i
 handler.group = true
 export default handler
